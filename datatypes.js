4df2ae16320773585edbf7b0ca290b621d8673fb
Muhammad Shahzad Diyal <shahzaddiyal786@gmail.com>
2025-01-25 10:28:43 +0500

    datatypes concepts

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat as updated version of javascript
+
+let name = "shahzad diyal"
+let age =19
+let isLoggedIn = false
+
+//null => standalone value .. it is an object
+//number => 2 to power 53 
+
+console.log(typeof null) 
+//output is an object
+
+console.log(typeof undefined)
+//output is an undefined
