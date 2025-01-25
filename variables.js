7d6821ff9358cf5d0e1516d88ab51c01d9fc6a58
Muhammad Shahzad Diyal <shahzaddiyal786@gmail.com>
2025-01-25 10:14:07 +0500

    variables and their difference

---
+++ variables.js
@@ -0,0 +1,11 @@
+//const variables cannot be updated or re-declared
+const accountId = 12344
+
+// var variables may both be updated and re-declared inside their scope.
+var email_Id = "sh@g.com"
+
+// Let variables can be updated but not re-declared
+let address = "paharpur"
+
+console.log(accountId)
+console.table([accountId,email_Id,address])
