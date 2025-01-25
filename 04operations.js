4c813de60e5161a39a4089025dd6e75e74b79cf3
Muhammad Shahzad Diyal <shahzaddiyal786@gmail.com>
2025-01-25 11:04:59 +0500

    different operations in javascript

---
+++ 04operations.js
@@ -0,0 +1,15 @@
+//Operation in javascipt
+//short key to write console.log is "clg"
+console.log(2+2) //output is 4
+
+let str1="ab" 
+let str2="cd"
+console.log(str1+str2) //output is abcd
+
+console.log("1" + 2) //output is 12
+
+console.log("1"+"2") //output is 12
+
+console.log(1+"2") //output is 12
+console.log(1+"2"+2) //output is 122
+console.log(1+2+"2") //output is 32
