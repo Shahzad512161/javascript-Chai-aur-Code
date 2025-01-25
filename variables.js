12c2c5ef6ba80f677f562660e1f6a46bf26260ed
Muhammad Shahzad Diyal <shahzaddiyal786@gmail.com>
2025-01-25 10:15:33 +0500

    updated variables

--- variables.js
+++ variables.js
@@ -2,10 +2,10 @@
 const accountId = 12344
 
 // var variables may both be updated and re-declared inside their scope.
-var email_Id = "sh@g.com"
+var email_Id = "shehzaddiyal161@gmail.com"
 
 // Let variables can be updated but not re-declared
-let address = "paharpur"
+let phone = "+92 0348-4567161"
 
 console.log(accountId)
 console.table([accountId,email_Id,address])
