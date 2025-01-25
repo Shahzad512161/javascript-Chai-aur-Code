200d7332a95394e452a9267da4e0097653befa76
Muhammad Shahzad Diyal <shahzaddiyal786@gmail.com>
2025-01-25 10:37:23 +0500

    type conversion in js

---
+++ 03conversions.js
@@ -0,0 +1,12 @@
+let score = "33asdfhk"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let isNumber = Number(score)
+console.log(isNumber)
+
+//"33" => 33
+//"33abc" => NaN
+
+//false => 0 , true =>1
